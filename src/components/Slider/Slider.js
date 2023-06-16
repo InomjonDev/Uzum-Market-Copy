615ccef6c9ca951c51d2bed0622de0ffd1d0e349
InomjonDev <inomjonismanaliev@gmail.com>
2023-06-16 23:12:20 +0500

    Initial Commit

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import { Navigation } from "swiper";
+import { Pagination } from "swiper";
 import bg1 from "../../assets/slider/bg1.jpg";
 import bg2 from "../../assets/slider/bg2.jpg";
 import bg3 from "../../assets/slider/bg3.jpg";
 import bg4 from "../../assets/slider/bg4.jpg";
 import "./Slider.css";
+import "swiper/css";
+import "swiper/css/pagination";
 
 function Slider() {
   return (
     <div className="container">
       <div className="slider__container">
-        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
           <SwiperSlide>
             <img src={bg1} alt="" />
           </SwiperSlide>
