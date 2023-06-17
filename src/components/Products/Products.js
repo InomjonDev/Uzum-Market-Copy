b358801bb755d15ba0011368d03ed0f06fc6d546
InomjonDev <inomjonismanaliev@gmail.com>
2023-06-17 21:16:56 +0500

    Skeleton is ready

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Skeleton from "../Skeleton/Skeleton";
 import { PRODUCTS } from "../../static";
 import "./Products.css";
 import ProductWrapper from "../product-wrapper/ProductWrapper";
 
 function Products() {
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsDataLoaded(true);
+    }, 2000);
+  }, []);
+
   return (
     <div className="container">
+      {!isDataLoaded && <Skeleton />}{" "}
+      {/* Показать скелетон, если данные не загружены */}
       <ProductWrapper data={PRODUCTS} />
       <h2>Products</h2>
-      <Skeleton />
     </div>
   );
 }
 
 export default Products;
+
+// import React from "react";
+// import Skeleton from "../Skeleton/Skeleton";
+// import { PRODUCTS } from "../../static";
+// import "./Products.css";
+// import ProductWrapper from "../product-wrapper/ProductWrapper";
+
+// function Products() {
+//   return (
+//     <div className="container">
+//       {!PRODUCTS.length && <Skeleton />} <ProductWrapper data={PRODUCTS} />
+//       <h2>Products</h2>
+//     </div>
+//   );
+// }
+
+// export default Products;
